9ba9d1dff29c6acfe6e16b21b07fa5fa82591cfa
Gerhard Burger <burger.ga@gmail.com>
2017-07-29 13:12:55 +0200

    Add better vi plugin

---
+++ better-vi-mode/better-vi-mode.plugin.zsh
@@ -0,0 +1,58 @@
+# Original file from oh-my-zsh/plugins/vi-mode
+
+# Updates editor information when the keymap changes.
+function zle-keymap-select() {
+  zle reset-prompt
+  zle -R
+}
+
+# Ensure that the prompt is redrawn when the terminal size changes.
+TRAPWINCH() {
+  zle &&  zle -R
+}
+
+zle -N zle-keymap-select
+zle -N edit-command-line
+
+
+bindkey -v
+
+# fix long delay (https://superuser.com/a/648046/143446)
+export KEYTIMEOUT=1
+bindkey -M vicmd '^[' undefined-key
+
+# allow v to edit the command line (standard behaviour)
+autoload -Uz edit-command-line
+bindkey -M vicmd 'v' edit-command-line
+
+# allow ctrl-p, ctrl-n for history autocomplete (https://unix.stackexchange.com/a/273672/26211)
+bindkey '^P' up-history
+bindkey '^N' down-history
+
+# allow ctrl-h, ctrl-w, ctrl-? for char and word deletion (standard behaviour)
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+
+# allow ctrl-r to perform backward search in history
+bindkey '^r' history-incremental-search-backward
+
+# proper history search (https://unix.stackexchange.com/a/273672/26211)
+bindkey -M vicmd "/" vi-history-search-backward
+bindkey -M vicmd "?" vi-history-search-forward
+bindkey -M vicmd "n" vi-repeat-search
+bindkey -M vicmd "N" vi-rev-repeat-search
+
+# if mode indicator wasn't setup by theme, define default
+if [[ "$MODE_INDICATOR" == "" ]]; then
+  MODE_INDICATOR="%{$fg_bold[red]%}<%{$fg[red]%}<<%{$reset_color%}"
+fi
+
+function vi_mode_prompt_info() {
+  echo "${${KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
+}
+
+# define right prompt, if it wasn't defined by a theme
+if [[ "$RPS1" == "" && "$RPROMPT" == "" ]]; then
+  RPS1='$(vi_mode_prompt_info)'
+fi
